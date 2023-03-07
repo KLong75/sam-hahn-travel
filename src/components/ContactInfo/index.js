eb9e91a89548c334d2853ac78dd9f7777c420d73
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-03-07 14:52:36 -0600

    add image to about, position image

--- src/components/ContactInfo/index.js
+++ src/components/ContactInfo/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-
+// import from Material UI
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
 const ContactInfo = () => {
   return (
-    <div>
-      <h5>Contact Info</h5>
-      <p>Phone: 555-555-5555</p>
-      
-      <a href="mailto:sam@samhahntravel.com "><p>Email: sam@samhahntravel.com</p></a>
-    </div>
+    <Grid container justifyContent='center'>
+      <Grid xs={12}>
+        <h5>Contact Info</h5>
+      </Grid>
+      <Grid xs={6} md={3}>
+        <p>Phone: 555-555-5555</p>
+      </Grid>
+      <Grid xs={6} md={3}>
+        <a href="mailto:sam@samhahntravel.com">
+          <p>Email: sam@samhahntravel.com</p>
+        </a>
+      </Grid>
+    </Grid>
   );
 };
 
